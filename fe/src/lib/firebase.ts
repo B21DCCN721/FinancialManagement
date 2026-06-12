23d05c54518ecd93f06b5df3cfc88920bb4f1f9a
B21DCCN721 <daoxuantri2003@gmail.com>
2026-06-12 11:02:54 +0700

    chore(fe): move firebase config to env variables

--- fe/src/lib/firebase.ts
+++ fe/src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBO_iy0QdB0E7O4toqmDVJdZzwHYVd0edM",
-  authDomain: "financialmanagement-498504.firebaseapp.com",
-  projectId: "financialmanagement-498504",
-  storageBucket: "financialmanagement-498504.firebasestorage.app",
-  messagingSenderId: "680792295793",
-  appId: "1:680792295793:web:2873635b130ba97dbf0ed0",
-  measurementId: "G-213CC1466B"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
